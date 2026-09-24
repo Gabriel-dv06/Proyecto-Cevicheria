49a1aab403a39220b856b13c7c67a38dd69a747b
gabo-0312 <u21209609@utp.edu.pe>
2026-09-23 23:44:39 -0500

    Subiendo el backend

---
+++ src/main/java/com/example/cevig/CevigApplication.java
@@ -0,0 +1,13 @@
+package com.example.cevig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CevigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CevigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cevig/CevigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cevig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CevigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
